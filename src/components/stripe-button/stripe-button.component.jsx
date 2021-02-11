a1e9ed48b150f3e715001e0df96599649e867856
Derek Gardiner <derekgardiner@Derek-Gardiners-MacBook-Pro.local>
2021-02-11 19:22:35 +0000

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({price}) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51IJjkiFvHQJnV8bhW4KGpgnLTqdJ0Uqaf9uzT5i2gNaWtQ6I2MMk0YYWwy0XcAjD3E2R3N2WKwhvAgfjWKOfcF7700xmBwkh1t';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+    return (
+        <StripeCheckout 
+            label='Pay Now'
+            name='CRWN CLothing'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+}
+
+export default StripeCheckoutButton;
